903179f0da902d028f0b4d26f94dacf983cb6c5e
Mohsen Hasani <mohsen2hasani@gmail.com>
2018-08-07 11:32:29 +0430

    write some comments

--- FileSharingCreator/Program.cs
+++ FileSharingCreator/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.DirectoryServices.AccountManagement;
+using System.IO;
+using System.Security.AccessControl;
 
 namespace FileSharingCreator
 {
@@ -10,6 +10,57 @@ namespace FileSharingCreator
     {
         static void Main(string[] args)
         {
+            try
+            {
+                //You need set domain and admin user and admin password
+                using (var context = new PrincipalContext(ContextType.Domain, "Domain Name", "Administrator UserName", "Administrator Password"))
+                {
+                    using (var searcher = new PrincipalSearcher(new GroupPrincipal(context)))
+                    {
+                        string folderName = string.Empty;
+                        DirectoryInfo info;
+                        DirectorySecurity security;
+                        //I need to get all the groups that have "_Unit" in their names
+                        foreach (GroupPrincipal groupPrincipal in searcher.FindAll().Where(a => a.Name.Contains("_Unit")))
+                        {
+                            var groupName = groupPrincipal.Name;
+
+                            //Create folder for each groups and set permissions
+                            folderName = $@"E:\Share\{groupName}";
+
+                            if (!Directory.Exists(folderName))
+                                Directory.CreateDirectory(folderName);
+
+                            info = new DirectoryInfo(folderName);
+                            security = info.GetAccessControl();
+                            security.AddAccessRule(new FileSystemAccessRule(groupPrincipal.SamAccountName, FileSystemRights.Read, InheritanceFlags.ContainerInherit, PropagationFlags.None, AccessControlType.Allow));
+                            security.AddAccessRule(new FileSystemAccessRule(groupPrincipal.SamAccountName, FileSystemRights.Read, InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
+                            info.SetAccessControl(security);
+
+                            var members = groupPrincipal.Members.Where(a => a is UserPrincipal).ToList();
+
+                            //Create Folder for each memebers of this group and set permissions
+                            foreach (UserPrincipal member in members)
+                            {
+                                folderName = $@"E:\Share\{groupName}\{member.DisplayName}";
+                                if (!Directory.Exists(folderName))
+                                    Directory.CreateDirectory(folderName);
+
+                                info = new DirectoryInfo(folderName);
+                                security = info.GetAccessControl();
+                                security.AddAccessRule(new FileSystemAccessRule(member.SamAccountName, FileSystemRights.Modify, InheritanceFlags.ContainerInherit, PropagationFlags.None, AccessControlType.Allow));
+                                security.AddAccessRule(new FileSystemAccessRule(member.SamAccountName, FileSystemRights.Modify, InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
+                                info.SetAccessControl(security);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                Console.ReadKey();
+            }
         }
     }
 }
